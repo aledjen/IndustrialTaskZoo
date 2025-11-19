058612bd2af6b19c91241e494fbbcdb90835a2f8
aledjen <adjenic97@gmail.com>
2025-11-19 10:32:54 +0100

    Implement health check endpoint (#1)

---
+++ IndustrialTaskZooAPI/Controllers/HeathCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IndustrialTaskZooAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeathCheckController : ControllerBase
+    {
+        [HttpGet("/healthcheck")]
+        public IActionResult HealthCheck()
+        {
+            return Ok("OK");
+        }
+    }
+}
